9b29ac27a54311d8311172da2f031ab5966efc52
Shneider <awperganaa@gmail.com>
2021-01-11 21:57:58 +0800

    event linstener

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ var financeController = (function() {
 //Programminn holbogch controller
 var appController = (function(uiController, financeController) {
 
-document.querySelector(".add__btn").addEventListener("click", function(){
-    console.log("Linked!!");
-} )
+ document.querySelector(".add__btn").addEventListener("click", function(){
+  //1.Оруулах өгөгдлийг дэлгэцнээс олж авнав
+  //2.Олж аөсан өгөгдлүүдээ санхүүгийн контроллерт датмжуулж тэнд хадгална.
+  //3.Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана
+  //4.Төсвийг тооцоолно
+  //5.Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
+ });
+ document.addEventListener("keypress", function(event){
+     console.log('tovch daraglaa');
+
+
+ });
+ 
 })(uiController, financeController);
