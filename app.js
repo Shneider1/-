3d12135123f0e7a060c39d6230b64e3ecaa9934a
Shneider <awperganaa@gmail.com>
2021-01-12 12:55:15 +0800

    event listener holbloo

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ var financeController = (function() {
 })();
 //Programminn holbogch controller
 var appController = (function(uiController, financeController) {
-
- document.querySelector(".add__btn").addEventListener("click", function(){
-  //1.Оруулах өгөгдлийг дэлгэцнээс олж авнав
+    var ctrlAddItem = function(){
+  //1.Оруулах өгөгдлийг дэлгэцнээс олж авна
+  console.log("Delgest ogogdol awh heseg");
   //2.Олж аөсан өгөгдлүүдээ санхүүгийн контроллерт датмжуулж тэнд хадгална.
   //3.Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана
   //4.Төсвийг тооцоолно
   //5.Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
+    };
+ document.querySelector(".add__btn").addEventListener("click", function(){
+  ctrlAddItem();
  });
  document.addEventListener("keypress", function(event){
-     console.log('tovch daraglaa');
-
-
+     if(event.keyCode == 13 || event.which == 13)
+     {
+         ctrlAddItem();
+     }
  });
  
 })(uiController, financeController);
